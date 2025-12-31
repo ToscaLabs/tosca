f79be494ef33cf70481cccbc499bd9456ba126e0
Luni-4 <luni-4@hotmail.it>
2025-12-31 09:56:45 +0100

    tosca-drivers: Fix clippy lint

--- crates/tosca-drivers/src/bh1750.rs
+++ crates/tosca-drivers/src/bh1750.rs
@@ -22,9 +22,9 @@ const RESET: u8 = 0x07;
 const MTREG_HIGH: u8 = 0x40;
 const MTREG_LOW: u8 = 0x60;
 
-/// Minimum allowed MTreg value.
+/// Minimum allowed `MTreg` value.
 pub const MTREG_MIN: u8 = 31;
-/// Maximum allowed MTreg value.
+/// Maximum allowed `MTreg` value.
 pub const MTREG_MAX: u8 = 254;
 const DEFAULT_MTREG: u8 = 69; // Default per datasheet.
 
